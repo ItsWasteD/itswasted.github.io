fde24faf5c699934098a0d6fff23b1fc245ce7cc
David Roth <david.roth@students.bfh.ch>
2025-12-01 01:44:05 +0400

    Display windowcard for locked

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom";
 import type { CalendarWindowType } from "../models/CalendarWindow";
-import WindowRenderer from "./WindowRenderer";
 import WindowCard from "./WindowCard";
 
-export default function Calendar({ windows }: { windows: CalendarWindowType[] | null }) {
+export default function Calendar({
+	windows,
+}: {
+	windows: CalendarWindowType[] | null;
+}) {
 	return (
 		<>
 			<h1 className="text-center text-4xl/loose">Adventskalender</h1>
 			<div className="grid grid-cols-2 md:grid-cols-4 gap-6 p-4">
 				{windows?.map((window, index) =>
 					window.locked ? (
-						<WindowRenderer />
+						<WindowCard data={window} />
 					) : (
 						<Link key={index} to={`/window/${window.day}`}>
 							<WindowCard data={window} />
--- src/components/WindowCard.tsx
+++ src/components/WindowCard.tsx
@@ -5,16 +5,18 @@ export default function WindowCard({ data }: { data: CalendarWindowType }) {
 	return (
 		<div
 			style={{
-				backgroundImage: `url(${BACKEND_URL + "/images/" + data.thumbnailPath})`,
+				backgroundImage: `url(${
+					BACKEND_URL + "/images/" + data.thumbnailPath
+				})`,
 			}}
 			className={`${
 				data.locked ? "blur-sm" : ""
-			} aspect-square bg-contain bg-center rounded-lg shadow-md relative`}
+			} aspect-square bg-contain text-4xl bg-center rounded-lg shadow-md relative`}
 		>
 			<div
-				className={`absolute inset-0 bg-black/40 flex items-center justify-center text-white text-center text-wrap font-bold`}
+				className={`absolute text-4xl inset-0 bg-black/40 flex items-center justify-center text-white text-center text-wrap font-bold`}
 			>
-				{data.day + ": " + data.text}
+				{data.day}
 			</div>
 		</div>
 	);
